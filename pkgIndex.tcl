90efb44cff64b0c0e9fabc0e93777c4bb3ad0987
Danilo Chang <ray2501@gmail.com>
2023-12-15 02:12:54 +0800

    Update tclconfig files

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,5 +1,10 @@
+# -*- tcl -*-
+# Tcl package index file, version 1.1
 #
-# Tcl package index file
-#
-package ifneeded sndfile 1.0 \
-    [list load [file join $dir libsndfile1.0.so] sndfile]
+if {[package vsatisfies [package provide Tcl] 9.0-]} {
+    package ifneeded sndfile 1.0 \
+	    [list load [file join $dir libtcl9sndfile1.0.so] [string totitle sndfile]]
+} else {
+    package ifneeded sndfile 1.0 \
+	    [list load [file join $dir libsndfile1.0.so] [string totitle sndfile]]
+}
