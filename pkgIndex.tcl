3bbcdfa0b1571b281624db480280b6c3cd037293
Danilo Chang <ray2501@gmail.com>
2025-01-29 11:24:25 +0800

    Bump version to 1.2

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -2,9 +2,9 @@
 # Tcl package index file, version 1.1
 #
 if {[package vsatisfies [package provide Tcl] 9.0-]} {
-    package ifneeded sndfile 1.1 \
-	    [list load [file join $dir libtcl9sndfile1.1.so] [string totitle sndfile]]
+    package ifneeded sndfile 1.2 \
+	    [list load [file join $dir libtcl9sndfile1.2.so] [string totitle sndfile]]
 } else {
-    package ifneeded sndfile 1.1 \
-	    [list load [file join $dir libsndfile1.1.so] [string totitle sndfile]]
+    package ifneeded sndfile 1.2 \
+	    [list load [file join $dir libsndfile1.2.so] [string totitle sndfile]]
 }
